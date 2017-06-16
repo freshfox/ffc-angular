0f33c73ba0fa24a8c46fc789091bcf748cdbbcd2
Alexander Ott <contact@alexanderott.at>
2017-06-16 23:11:22 +0200

    Remove all, add current state for use as plain git submodule

--- dist/components/control-messages.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl } from "@angular/forms";
-import { ValidationMessageProvider } from "../index";
-export declare class ControlMessagesComponent {
-    private validatoinMessageProvider;
-    control: FormControl;
-    constructor(validatoinMessageProvider: ValidationMessageProvider);
-    readonly errorMessage: string;
-}
--- dist/components/spinner.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnDestroy, OnInit } from "@angular/core";
-export declare class SpinnerComponent implements OnInit, OnDestroy {
-    progress: number;
-    private interval;
-    ngOnInit(): void;
-    ngOnDestroy(): void;
-}
