24b2213bb33b22954af991be001cc9f07e72c248
Alexander Ott <contact@alexanderott.at>
2017-06-27 18:37:17 +0200

    Change let to const

--- components/control-messages.component.ts
+++ components/control-messages.component.ts
@@ -18,7 +18,7 @@ export class ControlMessagesComponent {
 
 	get errorMessage() {
 		if (this.control) {
-			for (let propertyName in this.control.errors) {
+			for (const propertyName in this.control.errors) {
 				if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
 					if (this.validationMessageProvider) {
 						return this.validationMessageProvider.getValidationMessage(propertyName, this.control.errors[propertyName]);
