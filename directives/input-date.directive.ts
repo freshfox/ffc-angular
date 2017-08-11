5b18d4d367c6b89246c2f15ce47ff430e2804f0b
Alexander Ott <contact@alexanderott.at>
2017-08-11 16:28:52 +0200

    Date picker directive now takes and outputs a date

--- directives/input-date.directive.ts
+++ directives/input-date.directive.ts
@@ -1,74 +1,89 @@
-import {OnInit, ElementRef, Directive, forwardRef} from "@angular/core";
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {Directive, ElementRef, forwardRef, OnDestroy, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import * as Pikaday from 'pikaday';
 import * as moment from 'moment';
 
 export const DATEPICKER_VALUE_ACCESSOR: any = {
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => DatePickerDirective),
-    multi: true
+	provide: NG_VALUE_ACCESSOR,
+	useExisting: forwardRef(() => DatePickerDirective),
+	multi: true
 };
 
 @Directive({
-    selector: 'input[ff-datepicker]',
-    providers: [DATEPICKER_VALUE_ACCESSOR],
-    host: {'(change)': 'onChange()', '(blur)': 'onTouched()'},
+	selector: 'input[ff-datepicker]',
+	providers: [DATEPICKER_VALUE_ACCESSOR],
+	host: {'(change)': 'onChange()', '(blur)': 'onTouched()'},
 })
-export class DatePickerDirective implements OnInit, ControlValueAccessor {
+export class DatePickerDirective implements OnInit, ControlValueAccessor, OnDestroy {
 
-    private picker: any;
-    private onChangeCallback: (_: any) => void = () => {
-    };
-    private format: string = 'DD.MM.YYYY';
+	private picker: any;
+	private format: string = 'DD.MM.YYYY';
+	private date: Date;
 
-    onTouched = () => {
-    };
+	private onChangeCallback: (_: any) => void = () => {
+	};
 
-    constructor(private el: ElementRef) {
-    }
 
-    ngOnInit() {
-        this.picker = new Pikaday({
-            field: this.el.nativeElement,
-            format: this.format,
-            i18n: {
-                months: moment.localeData().months(),
-                weekdays: moment.localeData().weekdays(),
-                weekdaysShort: moment.localeData().weekdaysShort()
-            }
-        } as any);
-        document.removeEventListener('keydown', this.picker._onKeyChange);
-    }
+	onTouched = () => {
+	};
 
-    ngOnDestroy() {
-        this.picker.destroy();
-    }
+	constructor(private el: ElementRef) {
+	}
 
-    writeValue(value: any): void {
-        if (value == undefined) {
-            value = new Date();
-        }
-        this.picker.setDate(value);
-        this.onChange();
-    }
+	ngOnInit() {
+		this.picker = new Pikaday({
+			field: this.el.nativeElement,
+			format: this.format,
+			i18n: {
+				months: moment.localeData().months(),
+				weekdays: moment.localeData().weekdays(),
+				weekdaysShort: moment.localeData().weekdaysShort()
+			}
+		} as any);
+		document.removeEventListener('keydown', this.picker._onKeyChange);
+	}
 
-    onChange() {
-        let val = this.el.nativeElement.value;
-        let momentInstance = moment(val, this.format);
+	ngOnDestroy() {
+		this.picker.destroy();
+	}
 
-        if (momentInstance.isValid()) {
-            val = momentInstance.format('YYYY-MM-DD');
-        }
+	writeValue(value: Date): void {
+		if (value === undefined || value === null) {
+			value = new Date();
+		}
+		this.setDate(value);
+		this.onChange();
+	}
 
-        this.onChangeCallback(val);
-    }
+	private setDate(date: Date) {
+		this.date = date;
+		this.picker.setDate(date);
+	}
 
-    registerOnChange(fn: (_: any) => {}): void {
-        this.onChangeCallback = fn;
-    }
+	onChange() {
+		let val = this.el.nativeElement.value;
+		const momentInstance = moment(val, this.format);
+		if (momentInstance.isValid()) {
+			const date = momentInstance.toDate();
+			this.date.setFullYear(date.getFullYear());
+			this.date.setMonth(date.getMonth());
+			this.date.setDate(date.getDate());
 
-    registerOnTouched(fn: () => {}): void {
-        this.onTouched = fn;
-    }
+			val = this.date;
+		} else {
+			val = new Date();
+			this.setDate(val);
+		}
+
+		this.onChangeCallback(val);
+	}
+
+	registerOnChange(fn: (_: any) => {}): void {
+		this.onChangeCallback = fn;
+	}
+
+	registerOnTouched(fn: () => {}): void {
+		this.onTouched = fn;
+	}
 
 }
