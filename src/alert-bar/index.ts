7872ddc9de7c5df088a4e371c27477ab010e2c48
Alexander Ott <contact@alexanderott.at>
2017-10-23 18:50:35 +0200

    Export more stuff

--- src/alert-bar/index.ts
+++ src/alert-bar/index.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AlertBarComponent} from './alert-bar.component';
 
+export * from './alert-bar.component';
 
 @NgModule({
 	imports: [CommonModule],
--- src/select/index.ts
+++ src/select/index.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SelectComponent} from './select.component';
 
+export * from './select.component';
 
 @NgModule({
 	imports: [CommonModule],
