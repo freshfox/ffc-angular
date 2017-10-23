557352d5ca5bc1c5114563d655e3e252a91d9e43
Alexander Ott <contact@alexanderott.at>
2017-10-23 18:11:28 +0200

    Move button to new module structure

---
+++ src/button/index.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {ButtonComponent} from './button.component';
+import {CommonModule} from '@angular/common';
+
+export * from './button.component';
+
+@NgModule({
+	imports: [CommonModule],
+	declarations: [ButtonComponent],
+	exports: [ButtonComponent],
+})
+export class FFButtonModule {
+}
--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,22 @@ import {FFIconModule} from './icon/index';
 import {FFSpinnerModule} from './spinner/index';
 import {FFModalModule} from './modal/index';
 import {FFCoreModule} from './core/index';
+import {FFButtonModule} from './button/index';
 
 export * from './core/index';
 export * from './table/index';
 export * from './spinner/index';
 export * from './icon/index';
 export * from './modal/index';
+export * from './button/index';
 
 const FF_MODULES = [
 	FFCoreModule,
 	FFTableModule,
 	FFIconModule,
 	FFSpinnerModule,
-	FFModalModule
+	FFModalModule,
+	FFButtonModule
 ];
 
 @NgModule({
