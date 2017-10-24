c8f63bfa0d240c7de1ab6bcf0a8279a506e0d480
Alexander Ott <contact@alexanderott.at>
2017-10-24 14:24:48 +0200

    Import FFCoreModule in FFInputModule

--- src/input/index.ts
+++ src/input/index.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {InputComponent} from './input.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FFCoreModule} from '../core/index';
 
 export * from './input.component';
 
 @NgModule({
-	imports: [CommonModule, FormsModule, ReactiveFormsModule],
+	imports: [CommonModule, FormsModule, ReactiveFormsModule, FFCoreModule],
 	declarations: [InputComponent],
 	exports: [InputComponent],
 })
