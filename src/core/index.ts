962b850d5a53600acb5aea078ae65bf574fd22a8
Alexander Ott <contact@alexanderott.at>
2017-11-02 17:13:21 +0100

    Move control messages component and validation message provider to input module

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,35 +1,13 @@
-import {ModuleWithProviders, NgModule, Provider} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FakeValidationMessageProvider, ValidationMessageProvider} from './validation-message-provider';
-import {ControlMessagesComponent} from './control-messages.component';
 
 export * from './helpers';
 export * from './form-helpers';
 export * from './translate-packaged-loader';
-export * from './validation-message-provider';
 export * from './form-validator';
-export * from './control-messages.component';
-
-export interface FFCoreModuleConfig {
-	validationMessageProvider?: Provider;
-}
 
 @NgModule({
 	imports: [BrowserModule],
-	declarations: [ControlMessagesComponent],
-	exports: [ControlMessagesComponent],
 })
 export class FFCoreModule {
-
-	static forRoot(config: FFCoreModuleConfig = {}): ModuleWithProviders {
-		return {
-			ngModule: FFCoreModule,
-			providers: [
-				config.validationMessageProvider || {
-					provide: ValidationMessageProvider,
-					useClass: FakeValidationMessageProvider
-				},
-			]
-		};
-	}
 }
--- src/input/index.ts
+++ src/input/index.ts
@@ -1,14 +1,34 @@
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule, Provider} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {InputComponent} from './input.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FFCoreModule} from '../core/index';
+import {FakeValidationMessageProvider, ValidationMessageProvider} from './validation-message-provider';
+import {ControlMessagesComponent} from './control-messages.component';
 
 export * from './input.component';
+export * from './control-messages.component';
+export * from './validation-message-provider';
+
+export interface FFInputModuleConfig {
+	validationMessageProvider?: Provider;
+}
 
 @NgModule({
 	imports: [CommonModule, FormsModule, ReactiveFormsModule],
-	declarations: [InputComponent],
-	exports: [InputComponent],
+	declarations: [InputComponent, ControlMessagesComponent],
+	exports: [InputComponent, ControlMessagesComponent],
 })
 export class FFInputModule {
+	static forRoot(config: FFInputModuleConfig = {}): ModuleWithProviders {
+		return {
+			ngModule: FFCoreModule,
+			providers: [
+				config.validationMessageProvider || {
+					provide: ValidationMessageProvider,
+					useClass: FakeValidationMessageProvider
+				},
+			]
+		};
+	}
 }
