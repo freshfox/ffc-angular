b2f009fe0fd778a123258e5c36a0685e305a4396
Alexander Ott <contact@alexanderott.at>
2018-01-11 14:16:24 +0100

    Add safe pipe to core module

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {Formatter} from './formatter';
 import {NumberPipe} from './number.pipe';
+import {SafePipe} from './safe.pipe';
 
 export * from './helpers';
 export * from './form-helpers';
@@ -9,12 +10,13 @@ export * from './translate-packaged-loader';
 export * from './form-validator';
 export * from './number.pipe';
 export * from './formatter';
+export * from './safe.pipe';
 
 @NgModule({
-	declarations: [NumberPipe],
+	declarations: [NumberPipe, SafePipe],
 	imports: [BrowserModule],
-	providers: [Formatter, NumberPipe],
-	exports: [NumberPipe]
+	providers: [Formatter, NumberPipe, SafePipe],
+	exports: [NumberPipe, SafePipe]
 })
 export class FFCoreModule {
 }
