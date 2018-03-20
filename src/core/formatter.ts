904540a213f8e11969ce2f207d0144dbe073b653
Alexander Ott <contact@alexanderott.at>
2018-03-20 14:54:40 +0100

    Correctly unformat numbers

--- src/core/formatter.ts
+++ src/core/formatter.ts
@@ -49,7 +49,7 @@ export class Formatter {
 	}
 
 	parseMoney(formatted: string): number {
-		let parsed = numeral.unformat(formatted);
+		let parsed = numeral().unformat(formatted);
 		if (typeof parsed == 'undefined') {
 			return 0;
 		}
