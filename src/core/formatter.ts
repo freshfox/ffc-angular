a9dbf62fad5a7088f05f1f544c66fcff3b8e96aa
Alexander Ott <contact@alexanderott.at>
2018-03-20 14:48:21 +0100

    Upgrade packages, replace numbro with numeral

--- src/core/formatter.ts
+++ src/core/formatter.ts
@@ -1,48 +1,25 @@
 import {TranslateService} from '@ngx-translate/core';
 import {Injectable} from '@angular/core';
 
-// Workaround for the fact that numbro doesn't have a default export
-// See https://github.com/rollup/rollup/issues/670#issuecomment-281139978
-import * as numbro_ from 'numbro';
-let numbro: any = (<any>numbro_).default || numbro_;
+import numeral from 'numeral';
+import de from 'numeral/locales/de';
 
-const numbroDELang = {
-	langLocaleCode: 'de',
-	cultureCode: 'de',
+numeral.register('locale', 'de-special', Object.assign(de, {
 	delimiters: {
 		thousands: '.',
 		decimal: ','
-	},
-	abbreviations: {
-		thousand: 'k',
-		million: 'm',
-		billion: 'b',
-		trillion: 't'
-	},
-	ordinal: function () {
-		return '.';
-	},
-	currency: {
-		symbol: '€',
-		position: 'postfix'
-	},
-	defaults: {
-		currencyFormat: ',4 a'
-	},
-	formats: {
-		fourDigits: '4 a',
-		fullWithTwoDecimals: ',0.00 $',
-		fullWithTwoDecimalsNoCurrency: ',0.00',
-		fullWithNoDecimals: ',0 $'
 	}
-};
-numbro.culture('de', numbroDELang);
+}));
 
 @Injectable()
 export class Formatter {
 
 	constructor(private translate: TranslateService) {
-		numbro.culture(this.translate.currentLang);
+		let locale = this.translate.currentLang;
+		if (locale === 'de') {
+			locale = 'de-special';
+		}
+		numeral.locale(locale);
 	}
 
 	amount(value: number, numberOfDecimals: number = 2, alwaysShowDecimals: boolean = true): string {
@@ -68,14 +45,15 @@ export class Formatter {
 			value = 0;
 		}
 
-		return numbro(value).format(format);
+		return numeral(value).format(format);
 	}
 
 	parseMoney(formatted: string): number {
-		let parsed = numbro().unformat(formatted);
+		let parsed = numeral.unformat(formatted);
 		if (typeof parsed == 'undefined') {
 			return 0;
 		}
+
 		return parsed;
 	}
 
