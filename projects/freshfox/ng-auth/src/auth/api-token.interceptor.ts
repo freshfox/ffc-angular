53ff7f5fc33c22889cfe17a8335c809bc080cced
Alexander Ott <alex@freshfox.at>
2019-12-16 18:12:15 +0100

    fix(auth) Use map in pipe

--- projects/freshfox/ng-auth/src/auth/api-token.interceptor.ts
+++ projects/freshfox/ng-auth/src/auth/api-token.interceptor.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {AuthService} from './auth.service';
-import {catchError} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -13,7 +13,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		return this.authService.getAuthToken()
-			.pipe(token => {
+			.pipe(map(token => {
 				if (!token) {
 					return next.handle(req);
 				}
@@ -22,7 +22,7 @@ export class AuthInterceptor implements HttpInterceptor {
 				const authReq = req.clone({headers: req.headers.set('Authorization', `Bearer ${token}`)});
 				// Pass on the cloned request instead of the original request.
 				return next.handle(authReq);
-			})
+			}))
 			.pipe(
 				catchError(error => {
 					console.log(error);
