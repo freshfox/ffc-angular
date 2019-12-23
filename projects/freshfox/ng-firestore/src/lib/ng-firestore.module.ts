b2996a628929657a1c9a4c7813c50cb0e963dab2
Dominic Bartl <dominic.bartl.at@gmail.com>
2019-12-23 15:05:35 +0100

    Export decorators from firestore project

--- projects/freshfox/ng-firestore/src/lib/ng-firestore.module.ts
+++ projects/freshfox/ng-firestore/src/lib/ng-firestore.module.ts
@@ -4,6 +4,7 @@ import {FirestoreStorage} from './storage';
 export * from './base.model';
 export * from './base.repo';
 export * from './storage';
+export * from './decorators';
 
 @NgModule({
 	declarations: [],
