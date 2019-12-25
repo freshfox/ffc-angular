f38227c649cd67a9758d7e687ce3c9cd243d42fb
Alexander Ott <alex@freshfox.at>
2019-12-25 17:52:45 +0100

    Fix spinner in button not working, disable button when loading

--- projects/freshfox/ng-core/src/button/button.component.ts
+++ projects/freshfox/ng-core/src/button/button.component.ts
@@ -1,15 +1,28 @@
-import {Component, HostBinding, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostBinding, Input} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
 	selector: 'button[ff-button]',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	template: `
         <div class="ff-button__text" [style.visibility]="loading ? 'hidden' : 'visible'">
             <ng-content></ng-content>
         </div>
-        <mat-spinner *ngIf="loading"></mat-spinner>
+        <mat-spinner *ngIf="loading" @fade></mat-spinner>
 	`,
+	animations: [
+		trigger('fade', [
+			state('in', style({opacity: 1})),
+			transition(':enter', [
+				style({opacity: 0}),
+				animate(300)
+			]),
+			transition(':leave',
+				animate(300, style({opacity: 0})))
+		])
+	],
 	host: {
-		'[attr.disabled]': 'disabled ? true : null',
+		'[attr.disabled]': '(disabled || loading) ? true : null',
 	}
 })
 export class ButtonComponent {
--- projects/freshfox/ng-core/src/button/index.ts
+++ projects/freshfox/ng-core/src/button/index.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {ButtonComponent} from './button.component';
 import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 export * from './button.component';
 
 @NgModule({
-	imports: [CommonModule, MatProgressSpinnerModule],
+	imports: [CommonModule, MatProgressSpinnerModule, BrowserAnimationsModule],
 	declarations: [ButtonComponent],
 	exports: [ButtonComponent],
 })
