4efc0db95c9411cddfc4c14923b1d6a3dad04bd9
Alexander Ott <alex@freshfox.at>
2020-01-13 11:32:56 +0100

    (auth) Remove background fade in effect

--- projects/freshfox/ng-auth/src/ui/public.component.ts
+++ projects/freshfox/ng-auth/src/ui/public.component.ts
@@ -1,34 +1,25 @@
 import {Component, HostBinding, Input} from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
 	selector: 'ff-public',
 	template: `
-		<div class="ff-auth__wrapper">
-			<img class="ff-auth__background" [@fadeIn] src="assets/images/login/bg.jpg">
+        <div class="ff-auth__wrapper">
+            <img class="ff-auth__background" src="assets/images/login/bg.jpg">
 
-			<div class="ff-auth__inner">
-				<div class="ff-auth__content-wrapper">
-					<div class="ff-auth__header">
-						<img src="assets/images/login/logo.svg" class="ff-auth__logo">
-						<p class="ff-auth__tagline">{{ 'login.tagline' | translate }}</p>
-					</div>
-					<div class="ff-auth__content">
-						<router-outlet *ngIf="routerMode"></router-outlet>
-						<ng-content></ng-content>
-					</div>
-				</div>
-			</div>
-		</div>
-	`,
-	animations: [
-		trigger('fadeIn', [
-			transition('void => *', [
-				style({opacity: 0}),
-				animate('0.2s .5s ease-out')
-			])
-		])
-	]
+            <div class="ff-auth__inner">
+                <div class="ff-auth__content-wrapper">
+                    <div class="ff-auth__header">
+                        <img src="assets/images/login/logo.svg" class="ff-auth__logo">
+                        <p class="ff-auth__tagline">{{ 'login.tagline' | translate }}</p>
+                    </div>
+                    <div class="ff-auth__content">
+                        <router-outlet *ngIf="routerMode"></router-outlet>
+                        <ng-content></ng-content>
+                    </div>
+                </div>
+            </div>
+        </div>
+	`
 })
 export class PublicComponent {
 
