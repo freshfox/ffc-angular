99da29948c0af057b70febe54e28dba09cbeb019
Alexander Ott <alex@freshfox.at>
2020-12-28 12:45:32 +0100

    Properly import modules for test app

--- projects/ffc-angular-test/src/app/app.module.ts
+++ projects/ffc-angular-test/src/app/app.module.ts
@@ -2,27 +2,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {FFButtonModule} from '../../../freshfox/ng-core/src/button';
-import {FFSnackbarModule} from '../../../freshfox/ng-core/src/snackbar';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {FFSidenavModule} from '../../../freshfox/ng-core/src/sidenav';
 import {TranslateModule} from '@ngx-translate/core';
 import {RouterModule} from '@angular/router';
-import {
-	FF_AUTH_UI_BACKGROUND_PATH,
-	FF_AUTH_UI_LOGO_PATH,
-	FF_AUTH_UI_TAGLINE,
-	FFAuthUserInterfaceModule
-} from '../../../freshfox/ng-auth/src/ui/auth-ui.module';
-import {FFAuthModule} from '../../../freshfox/ng-auth/src/auth/auth.module';
 import {AngularFireModule} from '@angular/fire';
-import {FFInputModule} from '@freshfox/ng-core';
 import {of} from 'rxjs';
-import {FFSelectModule} from '../../../freshfox/ng-core/src/select';
-import {FFFormControlValidationMessageModule, InputValidationMessageProvider} from '../../../freshfox/ng-core/src/validation-message';
 import {MatCardModule} from '@angular/material/card';
-import {ValidationProvider} from './validation-provider';
+import {FF_AUTH_UI_BACKGROUND_PATH, FF_AUTH_UI_LOGO_PATH, FF_AUTH_UI_TAGLINE} from '../../../freshfox/ng-auth/src/ui/tokens';
+import { FFButtonModule } from 'projects/freshfox/ng-core/src/button';
+import { FFSnackbarModule } from 'projects/freshfox/ng-core/src/snackbar';
+import {FFSelectModule} from '../../../freshfox/ng-core/src/select';
+import {FFFormControlValidationMessageModule} from '../../../freshfox/ng-core/src/validation-message';
+import {FFSidenavModule} from '../../../freshfox/ng-core/src/sidenav';
 
 @NgModule({
 	declarations: [
@@ -35,7 +27,7 @@ import {ValidationProvider} from './validation-provider';
 		ReactiveFormsModule,
 		MatSidenavModule,
 		MatCardModule,
-		RouterModule.forRoot([], { relativeLinkResolution: 'legacy' }),
+		RouterModule.forRoot([], {relativeLinkResolution: 'legacy'}),
 		AngularFireModule.initializeApp({
 			apiKey: '***REMOVED***',
 			authDomain: '***REMOVED***',
@@ -44,23 +36,18 @@ import {ValidationProvider} from './validation-provider';
 			storageBucket: '***REMOVED***.appspot.com',
 			messagingSenderId: '***REMOVED***'
 		}),
-		FFAuthUserInterfaceModule,
-		FFAuthModule,
 		FFSidenavModule,
 		FFButtonModule,
 		FFSnackbarModule,
-		FFInputModule,
 		FFSelectModule,
-		FFFormControlValidationMessageModule.forRoot({
-			validationMessageProvider: 	{
-				provide: InputValidationMessageProvider,
-				useClass: ValidationProvider,
-			}
-		})
+		FFFormControlValidationMessageModule.forRoot()
 	],
 	providers: [
 		{provide: FF_AUTH_UI_TAGLINE, useValue: of('hello')},
-		{provide: FF_AUTH_UI_BACKGROUND_PATH, useValue: 'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80'},
+		{
+			provide: FF_AUTH_UI_BACKGROUND_PATH,
+			useValue: 'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80'
+		},
 		{provide: FF_AUTH_UI_LOGO_PATH, useValue: 'https://upload.wikimedia.org/wikipedia/commons/3/31/Apple_logo_white.svg'},
 	],
 	bootstrap: [AppComponent]
