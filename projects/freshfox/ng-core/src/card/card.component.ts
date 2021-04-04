2ecb91c628329f0530c02771bbe0c70a41ff8cbd
Alexander Ott <alex@freshfox.at>
2021-04-04 16:32:44 +0200

    (core) Move card classes to root element

--- projects/freshfox/ng-core/src/card/card.component.ts
+++ projects/freshfox/ng-core/src/card/card.component.ts
@@ -4,18 +4,16 @@ import {ChangeDetectionStrategy, Component, HostBinding, Input, OnInit} from '@a
 	selector: 'ff-card',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	template: `
-		<div class="shadow rounded-md overflow-hidden">
-			<ng-content select="[top]"></ng-content>
-			<div class="bg-white" [ngClass]="{ 'px-4 py-5 sm:p-6': padding }">
-				<ng-content></ng-content>
-			</div>
-			<ng-content select="[bottom]"></ng-content>
+		<ng-content select="[top]"></ng-content>
+		<div class="bg-white" [ngClass]="{ 'px-4 py-5 sm:p-6': padding }">
+			<ng-content></ng-content>
 		</div>
+		<ng-content select="[bottom]"></ng-content>
 	`
 })
 export class CardComponent implements OnInit {
 
-	@HostBinding('class') clazz = 'ff-card block';
+	@HostBinding('class') clazz = 'ff-card block shadow rounded-md overflow-hidden';
 
 	@Input() padding = true;
 
