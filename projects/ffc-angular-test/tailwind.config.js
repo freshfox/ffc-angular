0728c8d6b2e8b0045ae8083e2e5ee48cea3a006e
Alexander Ott <alex@freshfox.at>
2021-05-16 17:23:11 +0200

    Properly integrate tailwind in test project

---
+++ projects/ffc-angular-test/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	mode: 'jit',
+	important: true,
+	darkMode: 'class',
+	purge: {
+		content: [
+			'./projects/**/*.{html,ts}',
+		]
+	},
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
